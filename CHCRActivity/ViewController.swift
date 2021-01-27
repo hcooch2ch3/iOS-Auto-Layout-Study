5703db45fb2dfa0d2b3b3902f723386de3d2fe38
SM <hcooch2ch3@gmail.com>
2021-01-27 13:58:25 +0900

    Compression Hugging Priority Activity

--- CHCRActivity/ViewController.swift
+++ CHCRActivity/ViewController.swift
@@ -7,13 +7,26 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UITableViewController {
 
+    @IBOutlet var nameLabels: [UILabel]!
+    @IBOutlet var addressLabels: [UILabel]!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
-
+    
+    override func tableView(_ tableView: UITableView, accessoryButtonTappedForRowWith indexPath: IndexPath) {
+        let row = indexPath.row
+        let addressLabel = addressLabels[row]
+        let nameLabel = nameLabels[row]
+        addressLabel.isHidden.toggle()
+        if addressLabel.isHidden {
+            nameLabel.setContentCompressionResistancePriority(UILayoutPriority(751), for: NSLayoutConstraint.Axis.horizontal)
+        } else {
+            nameLabel.setContentCompressionResistancePriority(UILayoutPriority(749), for: NSLayoutConstraint.Axis.horizontal)
+        }
+    }
 
 }
 
